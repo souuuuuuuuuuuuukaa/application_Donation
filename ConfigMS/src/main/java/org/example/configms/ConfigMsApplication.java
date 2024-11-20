8ce499577be5467fe5f5ef6666559b91de96f178
souuuuuuuuuuuuukaa <battahsoukaina@gmail.com>
2024-11-20 21:39:29 +0100

    commit

---
+++ ConfigMS/src/main/java/org/example/configms/ConfigMsApplication.java
@@ -0,0 +1,15 @@
+package org.example.configms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigMsApplication.class, args);
+	}
+
+}
---
+++ ConfigMS/src/test/java/org/example/configms/ConfigMsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.configms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
